d968645cc05e2c76a86071ae2e852d31ff9529f7
Survy <sarvagya.vaish7@gmail.com>
2017-01-16 00:06:43 -0500

    Compare replace fragment with launching activity

--- app/src/main/java/com/survy/slidingpanel/Page1Fragment.java
+++ app/src/main/java/com/survy/slidingpanel/Page1Fragment.java
@@ -7,11 +7,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.Toast;
 
 public class Page1Fragment extends Fragment {
 
-    Button nextPageButton;
+    Button launchActivity;
+    Button replaceFragment;
 
     public Page1Fragment() {
         // Required empty public constructor
@@ -23,12 +23,27 @@ public class Page1Fragment extends Fragment {
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.fragment_page1, container, false);
 
-        nextPageButton = (Button) rootView.findViewById(R.id.next_page_button);
-        nextPageButton.setOnClickListener(new View.OnClickListener() {
+        launchActivity = (Button) rootView.findViewById(R.id.launch_activity);
+        launchActivity.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getActivity(), Page2Activity.class);
                 startActivity(intent);
+                getActivity().overridePendingTransition(R.anim.enter_from_right, R.anim.exit_to_left);
+            }
+        });
+
+        replaceFragment = (Button) rootView.findViewById(R.id.replace_fragment);
+        replaceFragment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Page2Fragment fragment = new Page2Fragment();
+                getFragmentManager()
+                        .beginTransaction()
+                        .setCustomAnimations(R.anim.enter_from_right, R.anim.exit_to_left, R.anim.enter_from_left, R.anim.exit_to_right)
+                        .replace(R.id.page_content, fragment)
+                        .addToBackStack(null)
+                        .commit();
             }
         });
 
--- app/src/main/java/com/survy/slidingpanel/Page2Fragment.java
+++ app/src/main/java/com/survy/slidingpanel/Page2Fragment.java
@@ -1,17 +1,16 @@
 package com.survy.slidingpanel;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.Toast;
 
 public class Page2Fragment extends Fragment {
 
-    Button nextPageButton;
+    Button backButton;
 
     public Page2Fragment() {
         // Required empty public constructor
@@ -23,11 +22,20 @@ public class Page2Fragment extends Fragment {
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.fragment_page2, container, false);
 
-        nextPageButton = (Button) rootView.findViewById(R.id.previous_page_button);
-        nextPageButton.setOnClickListener(new View.OnClickListener() {
+        backButton = (Button) rootView.findViewById(R.id.previous_page_button);
+        backButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                getActivity().finish();
+                // Pop fragment off of back stack if it exists,
+                // or exit activity.
+                int backStackEntryCount = getFragmentManager().getBackStackEntryCount();
+                if (backStackEntryCount > 0) {
+                    getFragmentManager().popBackStack();
+                }
+                else {
+                    getActivity().finish();
+                    getActivity().overridePendingTransition(R.anim.enter_from_left, R.anim.exit_to_right);
+                }
             }
         });
 
